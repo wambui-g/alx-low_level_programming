44d37545f5670f760cf6c2a90d196fbd654f4ab8
wambui-g <grayciekamau03@gmail.com>
2023-04-25 19:43:34 +0000

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,30 +10,28 @@ void print_to_98(int n)
 {
 	int i, j;
 
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i != 98)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-			}
-
-			else if (i == 98)
-			{
-				_putchar(i + '0');
-				_putchar('\n');
-			}
+			_putchar(i + '0');
+			_putchar(',');
 		}
 	}
 
 	else if (n > 98)
 	{
-		for (j = n; j > 98; j--)
+		for (j = n; j >= 98; j--)
 		{
 			_putchar(j + '0');
 			_putchar(',');
 		}
 	}
+
+	else
+	{
+		_putchar(n + '0');
+	}
+
+	_putchar('\n');
 }
