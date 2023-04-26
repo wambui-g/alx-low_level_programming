60f809a635e9770528ba6f69f93e061e6a7435cf
wambui-g <grayciekamau03@gmail.com>
2023-04-26 18:50:29 +0000

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,6 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -28,7 +27,6 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		
 		else
 		{
 			printf("Day of the year: %d\n", day);
