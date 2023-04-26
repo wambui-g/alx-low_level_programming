bd509bf2fe235fb434a4788f73d1dd21deb009f4
wambui-g <grayciekamau03@gmail.com>
2023-04-26 08:47:26 +0000

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- sum of multiples of 3 and 5
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 0, sum = 0;
+
+	while (n < 1024)
+	{
+
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum = sum + n;
+		}
+		n++;
+	}
+}
