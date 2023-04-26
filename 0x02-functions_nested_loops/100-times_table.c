b6cebe6d1cfb2c0b5e786dc81ee3d1c36fe0f756
wambui-g <grayciekamau03@gmail.com>
2023-04-26 07:45:49 +0000

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,13 +2,14 @@
 
 /**
  * print_times_table- prints times table
- *  @n: number of the times table
- * 
+ * @n: number of the times table
+ *
  * Return: void
  */
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
