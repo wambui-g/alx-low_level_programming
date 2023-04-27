3ac7ced49bbd8d5520a85b88e55fda83a097bbca
wambui-g <grayciekamau03@gmail.com>
2023-04-27 12:44:19 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers- prints 0 to 9
+ *
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
