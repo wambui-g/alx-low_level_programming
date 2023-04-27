81a81fa5346b85086a1b140bd3e3b90a97f1574b
wambui-g <grayciekamau03@gmail.com>
2023-04-27 13:11:46 +0000

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers- prints all apart from 2 and 4
+ *
+ * Return: 0 (success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		while (i == 2 || i == 4)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n';)
+	}
+}
