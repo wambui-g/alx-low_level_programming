95d236c4a536ebc9f206cdd78aa3fdb48aec2b00
wambui-g <grayciekamau03@gmail.com>
2023-04-27 18:43:58 +0000

    new

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_line- prints a straight line
@@ -11,11 +12,17 @@ void print_line(int n)
 	int i;
 
 	if (n == 0)
+	{
 		_putchar('\n');
+	}
+
 	else
+	{
 		for (i = 0; i < n; i++)
 		{
 			_putchar(95);
 		}
+	}
+
 	_putchar('\n');
 }
