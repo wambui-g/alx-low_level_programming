d794c83520025cdc2e3388133d0531f0e6a8a60f
wambui-g <grayciekamau03@gmail.com>
2023-05-04 13:30:57 +0000

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *cap_string- capitalizes words
+ * @c: char to consider
+ *
+ * Return: char
+ */
+char *cap_string(char *c)
+{
+	int i = 0, a = 0, cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (c[a] != '\0')
+	{
+		while (i < cspc)
+		{
+			if ((a == 0 || c[a - 1] == spc[i]) && (c[a] >= 97 && c[a] <= 122))
+				c[a] -= 32;
+			i++;
+		}
+		a++;
+	}
+	return (c);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *leet- encodes string into 1337
+ * @c: character to consider
+ *
+ * Return: char
+ */
+char *leet(char *c)
+{
+	int i = 0, j = 0, k = 0;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	while (c[i] != '\0')
+	{
+		while (l[j] != '\0')
+		{
+			if (c[i] == l[j])
+			{
+				k = j;
+				c[i] = e[k];
+			}
+			j++;
+		}
+		i++
+	}
+	return (c)
+}
