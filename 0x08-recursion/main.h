3cb1f7b78bd180995bf789e353e117a9a758ac44
wambui-g <grayciekamau03@gmail.com>
2023-05-10 08:18:14 +0000

    zero

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ *
+ * Desc: headerfile
+ */
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_numbers(int n);
+
+#endif /* MAIN_H */
