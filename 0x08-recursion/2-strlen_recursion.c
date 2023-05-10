624dfaf226588e04e6ddeb8f72a4bdf1efae6099
wambui-g <grayciekamau03@gmail.com>
2023-05-10 09:05:46 +0000

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,9 @@ int _strlen_recursion(char *s)
 {
 	int n = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		n++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-
-	return (n);
+	return (1 + _strlen_recursion(s + 1));
 }
