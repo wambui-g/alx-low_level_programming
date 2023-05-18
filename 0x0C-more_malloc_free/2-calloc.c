2546b5b6c2969532aa56e8b660a85639f47761f9
wambui-g <grayciekamau03@gmail.com>
2023-05-18 09:34:34 +0000

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,4 +4,28 @@
 
 /**
  * _calloc- allocates memory for an array using malloc
- * @nmemb
+ * @nmemb: no of elements
+ * @size: size of element of array
+ *
+ * Return: pointer to memory allocated or NULL if fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+
+	return (p);
+}
