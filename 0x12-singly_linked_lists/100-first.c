d8b8a52a82ab4a0ef95f8e5b1f17d0d047f598d6
wambui-g <grayciekamau03@gmail.com>
2023-06-02 14:05:46 +0000

    5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,12 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first- entry point
+ * first- prints before main
  *
  * Return: void
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
