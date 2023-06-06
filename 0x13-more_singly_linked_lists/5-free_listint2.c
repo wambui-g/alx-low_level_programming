b8d6512454318139f652b68f6a33eb098b6eafc4
wambui-g <grayciekamau03@gmail.com>
2023-06-06 07:40:05 +0000

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2- frees list
+ * @head: list head
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	while (head != NULL)
+	{
+		new = (*head)->next;
+		free(*head);
+		*head = new;
+	}
+}
