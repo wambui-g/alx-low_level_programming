d9ef0e28c353d96d8cbb841f8aed09a4fb009bd0
wambui-g <grayciekamau03@gmail.com>
2023-06-12 16:46:52 +0000

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,14 +12,14 @@ int create_file(const char *filename, char *text_content)
 	int i, fp;
 	int j = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fp < 0)
 		return (-1);
 
-	while (text_content)
+	if (text_content)
 	{
 		while (text_content[j])
 			j++;
