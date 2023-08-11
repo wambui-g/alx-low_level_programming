ef1c2361f7ae41edcc3fac4bff42e3ad8f3c36ea
wambui-g <grayciekamau03@gmail.com>
2023-08-11 02:03:05 -0700

    all

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set- function that adds an element to the hash table
+ * @ht: hash table to update
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 for sucess 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -22,6 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
+
 		node = node->next;
 	}
 	return (NULL);
