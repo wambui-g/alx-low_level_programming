78e497c21db6338968d61e81e968bb9432e74def
wambui-g <grayciekamau03@gmail.com>
2023-08-10 23:43:59 -0700

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,4 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
 /**
  * key_index- function that gives you the index of a key
@@ -9,5 +9,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int index;
 
+	index = hash_djb2(key) % size;
+
+	return (index);
 }
