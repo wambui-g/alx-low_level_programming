5bd358d2f0a47596ccb453152e8842b530c10213
wambui-g <grayciekamau03@gmail.com>
2023-08-11 16:52:18 +0000

    6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,5 +8,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *node, *next;
 
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+
+		while (node != NULL)
+		{
+			next = node->next;
+			free(node->key);
+			free(node->value);
+			node = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
